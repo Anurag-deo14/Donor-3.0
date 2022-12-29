d3d771f6bb76d3eba6cc0146d5c00a874d368359
Aniket Kumar <94353513+Kraniket901@users.noreply.github.com>
2022-12-29 22:40:22 +0530

    Update Map.jsx

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -4,15 +4,14 @@ import file2 from "../../images/file2.png";
 
 const Map = () => {
   return (
-    <div  id='map'>
-                  <h3 className="text-white text-3xl text-center my-10">
+    <div  id='map' >
+                  <h3 className="text-white text-3xl text-center my-10 pt-16 lg:pt-20">
                   Cryptocurrency World Map – Search Interest by Country
           </h3>
           <div className='lg:px-[10rem] px-[1rem] '>
           <img className='my-[1rem]' src={file1} alt="" />
             <img className='my-[1rem]' src={file2} alt="" />
           </div>
-            
     </div>
   )
 }
