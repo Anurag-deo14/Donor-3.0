04040fec855c8537428664ce2576ad460396184e
Aniket Kumar <kraniket123654@gmail.com>
2023-01-01 15:33:36 +0530

    added icons

--- client/src/components/Map.jsx
+++ client/src/components/Map.jsx
@@ -2,7 +2,16 @@ import React from 'react'
 import file1 from "../../images/file1.png";
 import file2 from "../../images/file2.png";
 import "../components/Map.css";
-import anime from "../assets/anime.png"
+import logo1 from "../assets/logo1.png"
+import logo2 from "../assets/logo2.png"
+import logo3 from "../assets/logo3.png"
+import logo4 from "../assets/logo4.png"
+import logo5 from "../assets/logo5.png"
+import logo6 from "../assets/logo6.png"
+import logo7 from "../assets/logo7.png"
+import logo8 from "../assets/logo8.png"
+import logo9 from "../assets/logo9.png"
+import logo10 from "../assets/logo10.png"
 
 
 
@@ -22,52 +31,52 @@ const Map = () => {
                   <th>Percentage</th>
                 </tr>
                 <tr>
-                  <td className="icon-flex">1. <img src={anime} alt="" /> Bitcoin</td>
+                  <td className="icon-flex">1. <img src={logo1} alt="" /> Bitcoin</td>
                   <td><div id='one'></div></td>
                   <td>59.9%</td>
                 </tr>
                 <tr>
-                  <td className="icon-flex">2. <img src={anime} alt="" /> Ethereum</td>
+                  <td className="icon-flex">2. <img src={logo2} style={{backgroundColor:"white"}} alt="" /> Ethereum</td>
                   <td><div id='two'></div></td>
                   <td>13.1%</td>
                 </tr>
                 <tr>
-                  <td className="icon-flex">3. <img src={anime} alt="" /> Dogecoin</td>
+                  <td className="icon-flex">3. <img src={logo3} alt="" /> Dogecoin</td>
                   <td><div id='three'></div></td>
                   <td>7.8%</td>
                 </tr>
                 <tr>
-                  <td className="icon-flex">4. <img src={anime} alt="" /> XRP</td>
+                  <td className="icon-flex">4. <img src={logo4} style={{backgroundColor:"white"}}alt="" /> XRP</td>
                   <td><div id='four'></div></td>
                   <td>5.5%</td>
                 </tr>
                 <tr>
-                  <td className="icon-flex">5. <img src={anime} alt="" /> Tron</td>
+                  <td className="icon-flex">5. <img src={logo5} alt="" /> Tron</td>
                   <td><div id='five'></div></td>
                   <td>4.8%</td>
                 </tr>
                 <tr>
-                  <td className="icon-flex">6. <img src={anime} alt="" /> Cardano</td>
+                  <td className="icon-flex">6. <img src={logo6} alt="" /> Cardano</td>
                   <td><div id='six'></div></td>
                   <td>3.9%</td>
                 </tr>
                 <tr>
-                  <td className="icon-flex">7. <img src={anime} alt="" /> Solana</td>
+                  <td className="icon-flex">7. <img src={logo7} alt="" /> Solana</td>
                   <td><div id='seven'></div></td>
                   <td>1.5%</td>
                 </tr>
                 <tr>
-                  <td className="icon-flex">8. <img src={anime} alt="" /> Litecoin</td>
+                  <td className="icon-flex">8. <img src={logo8} alt="" /> Litecoin</td>
                   <td><div id='eight'></div></td>
                   <td>1.4%</td>
                 </tr>
                 <tr>
-                  <td className="icon-flex">9. <img src={anime} alt="" /> Binance Coin</td>
+                  <td className="icon-flex">9. <img src={logo9} alt="" /> Binance Coin</td>
                   <td><div id='nine'></div></td>
                   <td>1%</td>
                 </tr>
                 <tr>
-                  <td className="icon-flex">10. <img src={anime} alt="" /> Avalanche</td>
+                  <td className="icon-flex">10. <img src={logo10} alt="" /> Avalanche</td>
                   <td><div id='ten'></div></td>
                   <td>1%</td>
                 </tr>
