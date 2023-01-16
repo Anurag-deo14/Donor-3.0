419760e893dd6ab8044df5725ba1ede63a2d1d04
Anurag Deo <110685873+Anurag-deo14@users.noreply.github.com>
2023-01-16 19:11:38 +0530

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -2,7 +2,7 @@ import React, { useContext,useState } from "react";
 import { AiFillPlayCircle } from "react-icons/ai";
 import { SiEthereum } from "react-icons/si";
 import { BsInfoCircle } from "react-icons/bs";
-
+import { MdContentCopy } from "react-icons/md";
 import { TransactionContext } from "../context/TransactionContext";
 import { shortenAddress } from "../utils/shortenAddress";
 import { Loader } from ".";
@@ -44,7 +44,7 @@ const Welcome = () => {
             The World  <br /> needs your Help!!
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Donate crypto for the needy using Cryptex. <br /> Your donations and support mean something. It means you care, and it can bring a smile to someone’s face. Give your valuable donation to <button className="color-grey scale-50 hover:scale-75" onClick={() => {navigator.clipboard.writeText(this.state.textToCopy)}}>0x6Ea12D225cfa208EAC185717dBb7bEcf8e985933</button>
+            Donate crypto for the needy using Cryptex. <br /> Your donations and support mean something. It means you care, and it can bring a smile to someone’s face. Give your valuable donation to <MdContentCopy /> <button className="color-grey scale-75 hover:scale-90" onClick={() => {navigator.clipboard.writeText(this.state.textToCopy)}}>0x6Ea12D225cfa208EAC185717dBb7bEcf8e985933</button>
           </p>
           {!currentAccount && (
             <button
