7f3d7e086c2d8f7a2f184a3eaff3b185324f33f6
Anurag Deo <110685873+Anurag-deo14@users.noreply.github.com>
2023-01-16 18:42:53 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -6,6 +6,7 @@ const yoboi = {
 }
 
 const Footer = () => (
+   <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
   <div className="w-full flex md:justify-center justify-between items-center flex-col p-4 gradient-bg-footer">
     <div className="w-full flex sm:flex-row flex-col justify-between items-center my-4">
       <div className="flex flex-[0.5] justify-center items-center">
@@ -21,11 +22,11 @@ const Footer = () => (
     </div>
 
     <div className="flex justify-center items-center flex-col mt-5">
-      <p className="text-white text-sm text-center">Come join us and become a part of community to change the world</p>
+      <p className="text-white text-sm text-center">Come join us and become a part of community to change the World.</p>
       <p className="text-white text-sm text-center font-medium mt-2">anuragjee2020@gmail.com</p>
     </div>
 
-    <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5 " />
+   
 
     <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
       <p className="text-white text-left text-xs">copyright @anurag&aniket</p>
