b11b488e181a2298929d4434a74de2e8e73d3a3e
Aniket Kumar <94353513+Kraniket901@users.noreply.github.com>
2023-01-17 09:52:13 +0530

    Update Welcome.jsx

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -46,7 +46,7 @@ const Welcome = () => {
             The World  <br /> needs your Help!!
           </h1>
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
-            Donate crypto for the needy using Cryptex. <br /> Your donations and support mean something. It means you care, and it can bring a smile to someone’s face. Give your valuable donation to <p className="hover:text-blue-300  flex space-x-2 cursor-pointer"> <p className="text-xs">{text}</p> <button onClick={() => {
+            Donate crypto for the needy using Cryptex. <br /> Your donations and support mean something. It means you care, and it can bring a smile to someone’s face. Give your valuable donation to <p className="hover:text-blue-300  flex space-x-2 cursor-pointer pt-2"> <p className="text-xs">{text}</p> <button onClick={() => {
          navigator.clipboard.writeText(text);}}><FiCopy /></button> </p>
           </p>
           {!currentAccount && (
